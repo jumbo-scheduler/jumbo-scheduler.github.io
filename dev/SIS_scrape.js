6631cdb85831f22ed14a024e2ee99f271922585f
Henry Ty <56137629+HenryTy83@users.noreply.github.com>
2025-03-29 14:29:02 -0400

    class catalogue + website base

---
+++ dev/SIS_scrape.js
@@ -0,0 +1,31 @@
+/*
+    INSTRUCTIONS: Copy/paste into terminal. wait for errors to stop, then call read_classes()
+*/
+
+const class_table = document.getElementsByClassName("tfp_accordion_row cls-show-js");
+const class_directory = {}
+
+// uncollapse all rows
+document.getElementsByClassName("tfp-show-result-sect")[0].click()
+
+const show_more = document.getElementsByClassName("status")
+for (let row of show_more) {
+    if (row.tagName == 'A') row.click()
+}
+
+// scrape the data from each row
+const read_classes = () => {
+    for (let i = 0; i < class_table.length; i++) {
+        let row = class_table[i]
+        let class_name = row.children[0].innerText.split(" ")[0]
+        let attributes =  class_table[i].children[2].children[1].children[0].children[0].children[3].children[1].children[0].children[0].children[0].children[0].children[0].children.length > 1 ? class_table[i].children[2].children[1].children[0].children[0].children[3].children[1].children[0].children[0].children[0].children[0].children[0].children[1].innerText.slice(17).split(', ').map(x => x.split('\n').join().split(',').join('')) : []
+        let prereqs = class_table[i].children[2].children[1].children[0].children[0].children[3].children[1].children[0].children[0].children[0].children[0].children[2].children[1].innerText
+        class_directory[class_name] = {
+            name: row.children[0].innerText.split(" ").slice(1).join(' '),
+            attributes: attributes,
+            prereqs: prereqs
+        }
+    }
+
+    return JSON.stringify(class_directory, 4, ' ') 
+}
