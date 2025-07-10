0d5997620dc4ac28c0f600f2fe6d1fe44e30e0ee
Henry Ty <56137629+HenryTy83@users.noreply.github.com>
2025-07-10 11:04:41 -0400

    refactor for readability
    i hate this pls kill me

--- dev/parsePrereqs.js
+++ dev/parsePrereqs.js
@@ -1,114 +1,84 @@
 // welcome to regex hell
 // regex101.com my beloved
-const classNames = Object.keys(total_catalog)
+const classNames = Object.keys(total_catalog);
 
-function createNode(type, values) {
-    const output = {
-        type: type,
-        values: values
+const createPrereqParser = (regex, handler) => {
+    return { regex: regex, handler: handler };
+};
+const createNode = (type, value) => {
+    return { type: type, value: value };
+};
+
+const prereqPatterns = [
+    // the phrase "None"
+    createPrereqParser(/none/, (_) => null),
+
+    // booleans
+    createPrereqParser(/\&/),
+    createPrereqParser(/\|/),
+
+    // years
+    createPrereqParser(/freshm[ea]n/, (_) => createNode("YEAR", 0)),
+    createPrereqParser(/sophomore/, (_) => createNode("YEAR", 1)),
+    createPrereqParser(/sophomore/, (_) => createNode("YEAR", 2)),
+    createPrereqParser(/sophomore/, (_) => createNode("YEAR", 3)),
+
+    // department (wtf is an 80 character limit)
+    createPrereqParser(
+        /(aast|acl|afr|amer|anth|arb|arch|ast|bio|bioe|bme|cee|cer|ch|chbe|chem|chns|cis|cls|cs|cshd|cst|cvs|data|deij|dh|dig|dnc|drw|drwm|ds|ec|ecs|ed|eds|ee|em|en|ene|eng|enp|ent|env|es|exp|fib|fms|fr|ger|gis|gra|grac|grk|haa|heb|hist|idis|ilcs|ilvs|intr|ital|jpn|js|las|lat|ling|lst|math|mdia|me|mes|mse|mtl|mus|nu|ots|pai|paim|pe|phil|pht|phtm|phy|por|prt|ps|psy|rcd|rel|rs|rus|scp|smfa|soc|spn|sts|tcs|tml|tps|ucoe|ucpc|uep|vms|wgss|mdvl|ml|skt)/,
+        (match) => createNode("DEPARTMENT", match[1])
+    ),
+];
+
+// find regex, replace with replace
+const createPrereqSanitizer = (regex, replace) => {
+    return { regex: regex, replace: replace };
+};
+const textSanitizers = [
+    // numbers
+    createPrereqSanitizer(/ one /g, " 1 "),
+
+    // booleans
+    createPrereqSanitizer(/ and /g, " & "),
+    createPrereqSanitizer(/ or /g, " | "),
+
+    // remove
+    createPrereqSanitizer(/ prior /g, " "),
+    createPrereqSanitizer(/\| grad/g, ""),
+    createPrereqSanitizer(/\| graduate students/g, ""),
+    createPrereqSanitizer(/\| permission/g, ""),
+    createPrereqSanitizer(/\| equivalent/g, ""),
+];
+
+function parsePrereq(prereqText) {
+    // sanitize text
+    prereqText = prereqText.toLowerCase();
+    for (const sanitizer of textSanitizers)
+        prereqText = prereqText.replaceAll(sanitizer.regex, sanitizer.replace);
+    prereqText = prereqText.trim();
+
+    var pattern, match;
+    for (pattern of prereqPatterns) {
+        match = prereqText.match(pattern.regex);
+        if (match != null) return pattern.handler(match);
     }
-    // console.log(output)
-    return output
-}
 
-function parsePrereq(prereqText, debug=false) {
-    if (debug === true) console.log(prereqText) // theres some type casting stupidity happening
-    var match;
-
-    // find and replace
-    prereqText = prereqText
-        .replaceAll(/economics/gi, "EC")
-        .replaceAll(/physics/gi, "PHY")
-        .replaceAll(/education/gi, "ED")
-        .replaceAll(/sociology/gi, "SOC")
-
-    const noPrereqsRegex = /none|undergraduate students|smfa students only|co-requisite/gi
-    if (noPrereqsRegex.exec(prereqText) != null) return null // no prereqs
-
-    // ignore restrictions on major
-    const majorBanRegex = /(?:not open to .+ majors)|(?:major in)|(?:majors? only)|(?:minors? only)|(?:excludes grad students)/gi
-     if (majorBanRegex.exec(prereqText) != null) return null
-
-    // remove trailing "or"s (stuff like department consent fks up the matching)
-    const trailingOrRegex = /(.*)\s+or\s+(?:consent|(?:instructor )?permission|graduate students|equivalents?|grad)/gi
-    match = trailingOrRegex.exec(prereqText)
-    if (match != null) return parsePrereq(match[1])
-
-    // remove leading stuff 
-    const leadingRegex = /(?:(?:prior )?completion(?: of| or concurrent enrollment in)?)\s+(.*)/gi
-    match = leadingRegex.exec(prereqText)
-    if (match != null) return parsePrereq(match[1])
-
-    if (prereqText.length > 200) return null // too yappy
-
-    // YEARS
-    const yearRegex = /(?:(first year)|freshm[ea]n)|(sophomore)|(junior)|(senior)/gi
-    match = yearRegex.exec(prereqText)
-    if (match != null) {
-        var years = []
-        for (let i = 1; i < 4; i++) {
-            if (match[i] != undefined) years.push(createNode("YEAR", i))
-        }
-        return createNode("ONE OF", years)
-    }
-
-    // SHORTHAND. GOD WHO TF WROTE THIS FKING DATABASE WHY IS IT SO INCONSISTENT
-    // #region ==============
-    const numberOrRegex = /([a-z]{2,4}) ([0-9]+) or ([0-9]+)/gi // Arch 26 or 27
-    match = numberOrRegex.exec(prereqText) 
-    if (match != null) return createNode("ONE OF", [parsePrereq(`${match[1].toUpperCase()} ${match[2].padStart(4, '0')}`), parsePrereq(`${match[1].toUpperCase()} ${match[3].padStart(4, '0')}`)])
-
-    const numberAndRegex = /([a-z]{2,4}) ([0-9]+) (?:\&|and) ([0-9]+)/gi // PHY 11 & 12
-    match = numberAndRegex.exec(prereqText) 
-    if (match != null) return createNode("ALL OF", [parsePrereq(`${match[1].toUpperCase()} ${match[2].padStart(4, '0')}`), parsePrereq(`${match[1].toUpperCase()} ${match[3].padStart(4, '0')}`)])
-
-    // #endregion
-
-
-    const andRegex = /\s+and\s+/gi
-    if (andRegex.exec(prereqText) != null) return createNode("ALL OF", prereqText.split(andRegex).forEach(parsePrereq))
-
-
-    const orRegex = /\s+or\s+/gi
-    if (orRegex.exec(prereqText) != null) return createNode("ONE OF", prereqText.split(orRegex).forEach(parsePrereq))
-
-    // DEPARTMENT
-    // #region ==============
-    const anyDepartmentRegex = /(?:any|one)\s+(?:prior\s+)?(AAST|ACL|AFR|AMER|ANTH|ARB|ARCH|AST|BIO|BIOE|BME|CEE|CER|CH|CHBE|CHEM|CHNS|CIS|CLS|CS|CSHD|CST|CVS|DATA|DEIJ|DH|DIG|DNC|DRW|DRWM|DS|EC|ECS|ED|EDS|EE|EM|EN|ENE|ENG|ENP|ENT|ENV|ES|EXP|FIB|FMS|FR|GER|GIS|GRA|GRAC|GRK|HAA|HEB|HIST|IDIS|ILCS|ILVS|INTR|ITAL|JPN|JS|LAS|LAT|LING|LST|MATH|MDIA|ME|MES|MSE|MTL|MUS|NU|OTS|PAI|PAIM|PE|PHIL|PHT|PHTM|PHY|POR|PRT|PS|PSY|RCD|REL|RS|RUS|SCP|SMFA|SOC|SPN|STS|TCS|TML|TPS|UCOE|UCPC|UEP|VMS|WGSS|MDVL|ML|SKT)\s+course/gi
-    match = anyDepartmentRegex.exec(prereqText)
-    if (match != null) return createNode("DEPARTMENT", [match[1]])
-    // #endregion ==============
-
-    // CLASS
-    // #region=======
-    const classRegex = /([A-Z]+) ?([0-9]+)/gi // matches any class
-    match = classRegex.exec(prereqText)
-    if (match != null) {
-        const parsedClass = `${match[1].toUpperCase()}-${match[2].padStart(4, '0')}`
-        if (!classNames.includes(parsedClass)) console.error(`Class ${parsedClass} does not exist???`)
-        return createNode("CLASS", [parsedClass])
-    }
-    const slashRegex = /([A-Z]+)\/([A-Z]+) ([0-9]+)/g // ANTH 0040 or ANTH/BIO 0044 or consent
-    match = slashRegex.exec(prereqText)
-    if (match != null) {
-        return createNode('ONE OF', [parsePrereq(`${match[1]} ${match[3].padStart(4, '0')}`), parsePrereq(`${match[2]} ${match[3].padStart(4, '0')}`)])
-    }
-    // #endregion=====
-
-    throw new Error(`Could not parse the phrase "${prereqText}"`) // default
+    console.log(prereqText, pattern, match);
+    throw new Error(`Could not parse the phrase "${prereqText}"`); // default
 }
 
 function parseCatalogPrereqs() {
     for (const className in total_catalog) {
-        const subjectData = total_catalog[className]
-        
+        const subjectData = total_catalog[className];
+
         try {
-            const parsedPrereqs = parsePrereq(subjectData.prereqs)
-            subjectData.parsedPrereqs = parsedPrereqs
-        }
-        catch(err) {
-            throw new Error(`Could not parse "${subjectData.prereqs}", received ${err} for class "${className}"`)
+            const parsedPrereqs = parsePrereq(subjectData.prereqs);
+            subjectData.parsedPrereqs = parsedPrereqs;
+        } catch (err) {
+            throw new Error(
+                `Could not parse "${subjectData.prereqs}", received ${err} for class "${className}"`
+            );
         }
     }
 }
-parseCatalogPrereqs()
+parseCatalogPrereqs();
