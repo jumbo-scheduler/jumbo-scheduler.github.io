4837f2fc20f207f5bcdb0ebdebad754160a39c66
Henry Ty <56137629+HenryTy83@users.noreply.github.com>
2025-12-25 00:21:25 -0500

    refactoring index.html

--- scripts/degreeSheetBuilder.js
+++ scripts/degreeSheetBuilder.js
@@ -1,3 +1,9 @@
 /*************************************
  *                  TABS
  *************************************/
+var tabs = [];
+var selectedTab = null;
+
+$("#new-tab-button").on("click", () => {
+    console.log("click")
+})
