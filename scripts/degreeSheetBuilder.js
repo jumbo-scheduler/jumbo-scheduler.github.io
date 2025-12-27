ff0086f827ac7718c14f0eb6248436d9589f614a
Henry Ty <56137629+HenryTy83@users.noreply.github.com>
2025-12-27 15:36:50 -0500

    every single button now works

--- scripts/degreeSheetBuilder.js
+++ scripts/degreeSheetBuilder.js
@@ -1,78 +1,9 @@
 /*************************************
- *                  TABS
- *************************************/
-
-let numTabs = 0;
-let numAlerts = 0;
-
-const createTab = () => {
-    // make a new tab, append to div and make it selected
-
-    // if too many say fuck you
-    if (numTabs > 9) {
-        if (numAlerts > 0 && Math.random() * 5 < 1) {
-            alert("Why does blud need so many minors 🤨📸");
-        } else {
-            alert("Blud thinks they're einstein taking more than 10 majors and minors");
-        }
-        numAlerts++;
-        return;
-    }
-    numTabs++;
-
-    // find the lowest index that doesn't cause a collision
-    const tabNames = $(".tab-label").get().map(x => $(x).text())
-    var newTabIndex = 1
-    if (tabNames.length > 0) {
-        while (tabNames.includes(`Major ${newTabIndex}`)) newTabIndex++
-    }
-
-    const tabTemplate = 
-    `<div class="tab">
-        <p class="tab-label">Major ${newTabIndex}</p>
-        <button class="close-tab-button">
-            <span style="font-family: monospace">✖</span>
-        </button>
-    </div>`
-
-    const newTab = $(tabTemplate)
-    newTab.on("click", () => {
-        $(".tab").removeClass("tab-selected")
-        newTab.addClass("tab-selected")
-
-        // insert code for showing the current tab here
-    })
-
-    newTab.children(".close-tab-button").on("click", () => {
-        if ($(".tab").length > 1) {
-            if (newTab.hasClass("tab-selected")) {
-                var nextOfKin // will select after this tab dies
-                // pick the tab directly after unless it's the last one
-                if (newTab.next().hasClass("tab")) nextOfKin = newTab.next()
-                else nextOfKin = newTab.prev() 
-
-                newTab.remove()
-                nextOfKin.click() // we are so sorry for your loss
-            }
-            else newTab.remove()
-            numTabs--;
-        }
-    })
-
-    newTab.insertBefore($("#new-tab-button"))
-    newTab.click()
-}
-createTab()
-
-$("#new-tab-button").on("click", createTab)
-
-
-
-/*************************************
  *           DEGREE SHEET
  *************************************/
 
 // JQUERY UI AUTOCOMPLETE
+// JOON CHANGE ME PLS
 $(function() {
     let classesArr = Object.keys(total_catalog).map(key => total_catalog[key].name);
     let deptsArr = Object.keys(departmentsPlaintext).map(key => `${key}: ${departmentsPlaintext[key]}`);
@@ -87,3 +18,60 @@ $(function() {
         source: allAttributes
     });
 });
+
+// new requirement buttons
+$("#ds-new-class").on("click", () => { 
+    const newRequirement = 
+        $(`<div class="ds-item ds-item-class">
+                <img class="ds-item-marker" src="img/class_marker.svg" />
+                <div class="ds-item-box">
+                    <input placeholder="Search or select class name" />
+                    <button class="ds-item-dup"></button>
+                    <button class="ds-item-move"></button>
+                    <button class="ds-item-delete"></button>
+                </div>
+            </div>`)
+
+    newRequirement.appendTo($("#ds-items-container"))                
+    currentTab.addRequirement(newRequirement);
+})
+
+$("#ds-new-dept").on("click", () => { 
+    const newRequirement = 
+        $(`<div class="ds-item ds-item-dept">
+                <img class="ds-item-marker" src="img/dept_marker.svg" />
+                <div class="ds-item-box">
+                    <input placeholder="Search or select department name" />
+                    <button class="ds-item-dup"></button>
+                    <button class="ds-item-move"></button>
+                    <button class="ds-item-delete"></button>
+                </div>
+            </div>`)
+
+    newRequirement.appendTo($("#ds-items-container"))                
+    currentTab.addRequirement(newRequirement);
+})
+
+$("#ds-new-attr").on("click", () => { 
+    const newRequirement = 
+        $(`<div class="ds-item ds-item-attr">
+                <img class="ds-item-marker" src="img/attr_marker.svg" />
+                <div class="ds-item-box">
+                    <input placeholder="Search or select attribute name" />
+                    <button class="ds-item-dup"></button>
+                    <button class="ds-item-move"></button>
+                    <button class="ds-item-delete"></button>
+                </div>
+            </div>`)
+
+    newRequirement.appendTo($("#ds-items-container"))                
+    currentTab.addRequirement(newRequirement);
+})
+
+$("#ds-new-group").on("click", () => { 
+    const newRequirement = 
+        $(`<p> JOON CHANGE ME </p>`)
+
+    newRequirement.appendTo($("#ds-items-container"))                
+    currentTab.addRequirement(newRequirement);
+})
---
+++ scripts/tab.js
@@ -0,0 +1,143 @@
+/*************************************
+ *                  TABS
+ *************************************/
+
+let numTabs = 0;
+let numAlerts = 0;
+
+class Tab {
+    constructor(name) {
+        this.name = name;
+        this.requirements = [] // pointer to each html requirement element
+    }
+
+    addRequirement(reqElement) {
+        this.requirements.push(reqElement);
+    }
+
+    display() {
+        // display all requirements in degree sheet
+        for (let req of this.requirements) {
+            $("#ds-items-container").append(req)
+        }
+    }
+}
+
+const tabs = []
+var currentTab = null;
+
+const onTabChange = (newTab) => {
+    currentTab = newTab || currentTab
+
+    // update degree sheet to reflect current tab
+    $("#ds-major-header h2").text(currentTab.name)
+
+    // clear previous requirements 
+    $("#ds-items-container").empty()
+
+    // display new requirements
+    currentTab.display()
+}
+
+const createTab = () => {
+    // make a new tab, append to div and make it selected
+
+    // if too many say fuck you
+    if (numTabs > 9) {
+        if (numAlerts > 0 && Math.random() * 5 < 1) {
+            alert("Why does blud need so many minors 🤨📸");
+        } else {
+            alert("Blud thinks they're einstein taking more than 10 majors and minors");
+        }
+        numAlerts++;
+        return;
+    }
+    numTabs++;
+
+    // find the lowest index that doesn't cause a collision
+    const tabNames = $(".tab-label").get().map(x => $(x).text())
+    var newTabIndex = 1
+    if (tabNames.length > 0) {
+        while (tabNames.includes(`Major ${newTabIndex}`)) newTabIndex++
+    }
+
+    const tabTemplate = 
+    `<div class="tab">
+        <p class="tab-label">Major ${newTabIndex}</p>
+        <button class="close-tab-button">
+            <span style="font-family: monospace">✖</span>
+        </button>
+    </div>`
+
+    const newTab = $(tabTemplate)
+    newTab.on("click", () => {
+        $(".tab").removeClass("tab-selected")
+        newTab.addClass("tab-selected")
+
+        onTabChange(tabs[$(".tab").get().indexOf(newTab.get(0))])
+    })
+
+    newTab.children(".close-tab-button").on("click", () => {
+        if ($(".tab").length > 1) {
+            // remove from tabs array
+            const tabIndex = $(".tab").get().indexOf(newTab.get(0))
+            tabs.splice(tabIndex, 1)
+
+            if (newTab.hasClass("tab-selected")) {
+                var nextOfKin // will select after this tab dies
+                // pick the tab directly after unless it's the last one
+                if (newTab.next().hasClass("tab")) nextOfKin = newTab.next()
+                else nextOfKin = newTab.prev() 
+
+                newTab.remove()
+                nextOfKin.click() // we are so sorry for your loss
+            }
+            else newTab.remove()
+            numTabs--;
+        }
+    })
+
+    // create the corresponding Tab object
+    currentTab = new Tab(`Major ${newTabIndex}`)
+    tabs.push(currentTab)
+
+    newTab.insertBefore($("#new-tab-button"))
+    newTab.click()
+}
+createTab()
+
+$("#new-tab-button").on("click", createTab)
+
+// rename tab
+$(".ds-rename-btn").on("click", () => {
+    const newName = prompt("Enter new name for this major/minor (JOON UN-UGLY THIS PLS):", $(".tab-selected").children(".tab-label").text())
+    if (newName != null && newName.trim() != "") {
+        $(".tab-selected").children(".tab-label").text(newName.trim())
+        // also rename the Tab object
+        const tabIndex = $(".tab").get().indexOf($(".tab-selected").get(0))
+        tabs[tabIndex].name = newName.trim()
+
+        // also rename the degree sheet header
+        $("#ds-major-header h2").text(newName.trim())
+    }
+})
+
+// collapse/expand degree sheet
+$(".ds-collapse-btn").on("click", () => {
+    if ($("#ds-items-container").css("max-height") != "0px") {
+        // shrink ds items container height with animation
+        $("#ds-items-container")
+            .css("max-height", "0vh")
+        // rotate arrow with animation
+        $(".ds-collapse-btn img")
+            .css("transform", "rotate(-90deg)")
+    }
+    else {
+        // expand ds items container height with animation
+        $("#ds-items-container")
+            .css("max-height", "100vh")
+        // rotate arrow with animation
+        $(".ds-collapse-btn img")
+            .css("transform", "rotate(0deg)")
+    }
+})
